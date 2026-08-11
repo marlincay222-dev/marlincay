5ab161627a30a6d0680d058a5cfed3fc27e0ee9b
watnot229-ops <watnot229@gmail.com>
2026-08-11 19:25:59 +0530

    Update preloader text to APTDC project

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -89,7 +89,7 @@ export default function Hero() {
           animate={{ opacity: 1, filter: "blur(0px)" }}
           transition={{ duration: 0.8, delay: 0.2 }}
         >
-          Your sea view dining experience
+          BEACH AND RESORTS | AN APTDC PROJECT
         </motion.p>
       </motion.div>
     </section>
